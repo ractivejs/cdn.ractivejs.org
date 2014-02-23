d1e47860bb14690e4859392627e213231317dab1
Travis-CI <richard.a.harris+travis@gmail.com>
2014-02-23 14:35:52 +0000

    Updated edge version

--- edge/Ractive-legacy.runtime.js
+++ edge/Ractive-legacy.runtime.js
@@ -1,6 +1,6 @@
 /*
 
-	Ractive - v0.4.0-pre - 2014-02-22
+	Ractive - v0.4.0-pre - 2014-02-23
 	==============================================================
 
 	Next-generation DOM manipulation - http://ractivejs.org
