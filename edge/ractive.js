88c2e7812c2142fe825f111298f050cccb7a6722
Travis-CI <richard.a.harris+travis@gmail.com>
2014-04-09 17:21:49 +0000

    Updated edge version

--- edge/ractive.js
+++ edge/ractive.js
@@ -1,6 +1,6 @@
 /*
 	Ractive.js v0.4.0
-	2014-04-09 - commit cdd25e46
+	2014-04-09 - commit 5de8cde6
 
 	http://ractivejs.org
 	http://twitter.com/RactiveJS
@@ -4228,78 +4228,71 @@
 		return DomInterpolator;
 	}( config_types, shared_teardown, render_shared_Mustache__Mustache, render_DomFragment_shared_detach );
 
-	var render_DomFragment_Section_prototype_merge = function() {
-
-		var toTeardown = [];
-		return function sectionMerge( newIndices ) {
-			var section = this,
-				parentFragment, firstChange, i, newLength, reassignedFragments, fragmentOptions, fragment, nextNode;
-			parentFragment = this.parentFragment;
-			reassignedFragments = [];
-			// first, reassign existing fragments
-			newIndices.forEach( function reassignIfNecessary( newIndex, oldIndex ) {
-				var fragment, by, oldKeypath, newKeypath;
-				if ( newIndex === oldIndex ) {
-					reassignedFragments[ newIndex ] = section.fragments[ oldIndex ];
-					return;
-				}
-				if ( firstChange === undefined ) {
-					firstChange = oldIndex;
-				}
-				// does this fragment need to be torn down?
-				if ( newIndex === -1 ) {
-					toTeardown.push( section.fragments[ oldIndex ] );
-					return;
-				}
-				// Otherwise, it needs to be reassigned to a new index
-				fragment = section.fragments[ oldIndex ];
-				by = newIndex - oldIndex;
-				oldKeypath = section.keypath + '.' + oldIndex;
-				newKeypath = section.keypath + '.' + newIndex;
-				fragment.reassign( section.descriptor.i, oldIndex, newIndex, by, oldKeypath, newKeypath );
-				reassignedFragments[ newIndex ] = fragment;
-			} );
-			while ( fragment = toTeardown.pop() ) {
-				fragment.teardown( true );
-			}
-			// If nothing changed with the existing fragments, then we start adding
-			// new fragments at the end...
-			if ( firstChange === undefined ) {
-				firstChange = this.length;
-			}
-			this.length = newLength = this.root.get( this.keypath ).length;
-			if ( newLength === firstChange ) {
-				// ...unless there are no new fragments to add
+	var render_DomFragment_Section_prototype_merge = function sectionMerge( newIndices ) {
+		var section = this,
+			parentFragment, firstChange, i, newLength, reassignedFragments, fragmentOptions, fragment, nextNode;
+		parentFragment = this.parentFragment;
+		reassignedFragments = [];
+		// first, reassign existing fragments
+		newIndices.forEach( function reassignIfNecessary( newIndex, oldIndex ) {
+			var fragment, by, oldKeypath, newKeypath;
+			if ( newIndex === oldIndex ) {
+				reassignedFragments[ newIndex ] = section.fragments[ oldIndex ];
 				return;
 			}
-			// Prepare new fragment options
-			fragmentOptions = {
-				descriptor: this.descriptor.f,
-				root: this.root,
-				pNode: parentFragment.pNode,
-				owner: this
-			};
-			if ( this.descriptor.i ) {
-				fragmentOptions.indexRef = this.descriptor.i;
+			if ( firstChange === undefined ) {
+				firstChange = oldIndex;
 			}
-			// Add as many new fragments as we need to, or add back existing
-			// (detached) fragments
-			for ( i = firstChange; i < newLength; i += 1 ) {
-				// is this an existing fragment?
-				if ( fragment = reassignedFragments[ i ] ) {
-					this.docFrag.appendChild( fragment.detach( false ) );
-				} else {
-					fragmentOptions.context = this.keypath + '.' + i;
-					fragmentOptions.index = i;
-					fragment = this.createFragment( fragmentOptions );
-				}
-				this.fragments[ i ] = fragment;
+			// does this fragment need to be torn down?
+			if ( newIndex === -1 ) {
+				section.fragments[ oldIndex ].teardown( true );
+				return;
 			}
-			// reinsert fragment
-			nextNode = parentFragment.findNextNode( this );
-			parentFragment.pNode.insertBefore( this.docFrag, nextNode );
+			// Otherwise, it needs to be reassigned to a new index
+			fragment = section.fragments[ oldIndex ];
+			by = newIndex - oldIndex;
+			oldKeypath = section.keypath + '.' + oldIndex;
+			newKeypath = section.keypath + '.' + newIndex;
+			fragment.reassign( section.descriptor.i, newIndex, oldKeypath, newKeypath );
+			reassignedFragments[ newIndex ] = fragment;
+		} );
+		// If nothing changed with the existing fragments, then we start adding
+		// new fragments at the end...
+		if ( firstChange === undefined ) {
+			firstChange = this.length;
+		}
+		this.length = newLength = this.root.get( this.keypath ).length;
+		if ( newLength === firstChange ) {
+			// ...unless there are no new fragments to add
+			return;
+		}
+		// Prepare new fragment options
+		fragmentOptions = {
+			descriptor: this.descriptor.f,
+			root: this.root,
+			pNode: parentFragment.pNode,
+			owner: this
 		};
-	}();
+		if ( this.descriptor.i ) {
+			fragmentOptions.indexRef = this.descriptor.i;
+		}
+		// Add as many new fragments as we need to, or add back existing
+		// (detached) fragments
+		for ( i = firstChange; i < newLength; i += 1 ) {
+			// is this an existing fragment?
+			if ( fragment = reassignedFragments[ i ] ) {
+				this.docFrag.appendChild( fragment.detach( false ) );
+			} else {
+				fragmentOptions.context = this.keypath + '.' + i;
+				fragmentOptions.index = i;
+				fragment = this.createFragment( fragmentOptions );
+			}
+			this.fragments[ i ] = fragment;
+		}
+		// reinsert fragment
+		nextNode = parentFragment.findNextNode( this );
+		parentFragment.pNode.insertBefore( this.docFrag, nextNode );
+	};
 
 	var render_shared_updateSection = function( isArray, isObject ) {
 
