8d0d95d973849e608f8fd75cc0dd18737c78391c
Travis-CI <richard.a.harris+travis@gmail.com>
2014-04-27 21:36:43 +0000

    Updated edge version

--- edge/ractive.js
+++ edge/ractive.js
@@ -1,6 +1,6 @@
 /*
 	ractive.js v0.4.0
-	2014-04-27 - commit 76600b31 
+	2014-04-27 - commit d40765e4 
 
 	http://ractivejs.org
 	http://twitter.com/RactiveJS
@@ -4064,7 +4064,7 @@
 	var render_shared_Mustache_resolve = function( types, registerDependant, unregisterDependant ) {
 
 		return function resolveMustache( keypath ) {
-			var i;
+			var reassignTarget;
 			// In some cases, we may resolve to the same keypath (if this is
 			// an expression mustache that was reassigned due to an ancestor's
 			// keypath) - in which case, this is a no-op
@@ -4074,14 +4074,16 @@
 			// if we resolved previously, we need to unregister
 			if ( this.registered ) {
 				unregisterDependant( this );
-				// is this a section? if so, we may have children that need
-				// to be reassigned
-				// TODO only DOM sections?
-				if ( this.type === types.SECTION ) {
-					i = this.fragments.length;
-					while ( i-- ) {
-						this.fragments[ i ].reassign( null, null, this.keypath, keypath );
-					}
+				//need to reassign the element, if this belongs to one, for keypath changes
+				if ( this.parentFragment && this.parentFragment.owner && this.parentFragment.owner.element ) {
+					reassignTarget = this.parentFragment.owner.element;
+				} else {
+					reassignTarget = this;
+				}
+				reassignTarget.reassign( null, null, this.keypath, keypath );
+				//if we already updated due to reassignent, we can exit
+				if ( keypath === this.keypath ) {
+					return;
 				}
 			}
 			this.keypath = keypath;
@@ -4971,6 +4973,7 @@
 			// be explicit when using two-way data-binding about what keypath you're
 			// updating. Using it in lists is probably a recipe for confusion...
 			if ( !interpolator.keypath ) {
+				//TODO: What about kx?
 				interpolator.resolve( interpolator.descriptor.r );
 			}
 			this.keypath = interpolator.keypath;
